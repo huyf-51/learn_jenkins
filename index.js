13e13aa60d25a8cf1faa2621d04bf32b88209ef8
Huy <dgh2003lqm@gmail.com>
2024-12-16 21:30:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var port = 3003;
+const mongoose = require('mongoose');
+
+mongoose
+    .connect('mongodb://root:example@127.0.0.1:5000/abc?authSource=admin')
+    .then(() => console.log('Connected!'));
+
+const User = new mongoose.Schema({
+    user: { type: String },
+    pass: { type: String },
+});
+
+const UserModel = mongoose.model('User', User);
+
+app.get('/', async (req, res) => {
+    const user = await UserModel.find();
+    console.log(user);
+    res.send('hello world');
+});
+
+app.listen(port, () => {
+    console.log(`app listen on port ${port}`);
+});
